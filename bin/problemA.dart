ea5024326abea3a626afb840c6b5a67a2f355b60
Mohamed-Awadin5 <mohamed.awadin6@gmail.com>
2023-07-16 02:29:13 +0300

    first commit

---
+++ bin/problemA.dart
@@ -0,0 +1,5 @@
+import 'package:sheet2/sheet2.dart' as sheet2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sheet2.calculate()}!');
+}
---
+++ test/sheet2_test.dart
@@ -0,0 +1,8 @@
+import 'package:sheet2/sheet2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
